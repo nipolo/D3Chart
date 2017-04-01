2b3d425e41541ceba5176f45270ba6f680907240
Borislav Valkov <bvalkov@dev.local>
2017-04-01 05:33:12 +0300

    studing-selections

--- src/app/components/d3-examples/simple-render-data.component.ts
+++ src/app/components/d3-examples/simple-render-data.component.ts
@@ -1,5 +1,6 @@
 import { Component, AfterViewInit } from '@angular/core';
 import * as d3 from 'd3'
+import * as R from 'ramda'
 import { ScaleLinear, ScaleTime } from 'd3'
 
 @Component({
@@ -20,6 +21,8 @@ export class SimpleRenderData implements AfterViewInit {
         //this.testSelectDomEl();
 
         this.testVisualizeData();
+        this.testSVGCircles();
+        this.testPolygon();
     }
 
     testLinearScale(): void {
@@ -64,25 +67,91 @@ export class SimpleRenderData implements AfterViewInit {
     }
 
     testVisualizeData(): void {
-        let scores: Array<{ name: string, score: number }> =
+        let scores: Array<any> =
             [
-                { name: 'Alice', score: 96 },
-                { name: 'Billy', score: 56 },
-                { name: 'Cindy', score: 76 },
                 { name: 'David', score: 100 },
                 { name: 'Emily', score: 83 },
             ];
 
-        let update = d3.select('div.chart')
-            .selectAll('div')
-            .data(scores);
+        let update = d3.select('div.chart').selectAll('div')
+            .data(scores, (x: any) => { console.log(x); return x ? x.name : null });
 
-        update.enter()
+        console.log(update);
+
+        let enter = update.enter()
             .append('div')
-            .text((d) => {
+            .text((d: any) => {
                 return d.name;
             })
             .style('color', 'red');
 
+        //update.exit().remove();
+
+        update.merge(update.exit())
+            .style('height', '50px')
+            .append('span')
+            .text('asd');
+        update.exit()
+            .append('span')
+            .text('gggasdsa');
+    }
+
+    testSVGCircles(): void {
+        d3.json('app/components/d3-examples/data/dataPoints.json', (data: [{ x: any, y: any, k: any }]) => {
+
+            let svg = d3.select('svg');
+            svg.selectAll("circle")
+                .data(data, (x: any) => { console.log(x.k); return x.k; })
+                .enter().append("circle")
+                .attr("cx", function (d) { return d.x; })
+                .attr("cy", function (d) { return d.y; })
+                .attr("r", 2.5);
+
+            data.push({ "x": 5, "y": 60, "k": "asdf" });
+
+            let data1 = [{ "x": 15, "y": 60, "k": "asdfffff" }];
+            let svg1 = d3.select('svg');
+            let newSvgData = svg1.selectAll("circle")
+                .data(data1, (x: any) => { console.log(x.k); return x.k; });
+
+            svg.selectAll("circle")
+                .attr("r", 10);
+
+            newSvgData.exit().remove();
+
+            newSvgData.enter().append("circle")
+                .attr("cx", function (d) { return d.x; })
+                .attr("cy", function (d) { return d.y; })
+                .attr("r", 5);
+
+            svg.selectAll("circle")
+                .data(data, (x: any) => { console.log(x.k); return x.k; })
+                .enter().append("circle")
+                .attr("cx", function (d) { return d.x; })
+                .attr("cy", function (d) { return d.y; })
+                .attr("r", 2.5);
+        });
+    }
+
+    testPolygon(): void {
+        let points = [
+            { x: 220, y: 10 },
+            { x: 300, y: 210 },
+            { x: 170, y: 250 },
+            { x: 123, y: 234 }
+        ];
+
+        let firstPoint: any = points.splice(0, 1)[0];
+        let strPoints = R.reduce((str, el: any) => str + ' ' + el.x + ',' + el.y,
+            firstPoint.x + ',' + firstPoint.y,
+            points
+        )
+
+        let polygon = d3.select("svg").selectAll("polygon").data([strPoints]);
+
+        polygon.enter()
+            .append('polygon')
+            .attr("points", function (d: string) { return <string>d; })
+            .attr("style", "fill:lime;stroke:purple;stroke-width:1");
     }
 }
